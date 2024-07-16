7e6e07a2695a5b40a8362652954e7af0fe5922f3
Junsung Cho <junsung@directional.net>
2024-07-16 13:52:31 +0900

    bump: version 0.0.2 → 0.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 group = "org.web3kt"
-version = "0.0.2"
+version = "0.0.3"
 
 plugins {
     id("org.springframework.boot") version "3.3.1"
